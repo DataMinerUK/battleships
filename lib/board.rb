579cd96ee7b58fb50977b9eecdcd000952d727d1
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-15 13:26:26 +0100

    Refactoring to initialize a blank board with all the possible coordinates set to 'water'

--- lib/board.rb
+++ lib/board.rb
@@ -1,17 +1,26 @@
 class Board
 
   attr_reader :dimension
+  attr_accessor :playing_on
 
   def initialize dimension
     @dimension = dimension
+    @playing_on = self.blank
   end
 
-  def place ship, coordinates
-    ship
+  def blank
+    # Making a Hash with all the coordinates of the board as keys setting them to value 'water'
+    coordinate_keys  = (0..@dimension[0]-1).to_a.product((0..@dimension[1]-1).to_a)
+    value_water = ['water'] * coordinate_keys.count
+    Hash[coordinate_keys.zip(value_water)]
   end
 
-  def ship?
+  def place ship, coordinate
+    playing_on[coordinate] = ship
+  end
 
-  end  
+  def ship? coordinate
+    playing_on[coordinate] != 'water'
+  end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,14 +2,20 @@ require './lib/board'
 
 describe Board do
 
-  subject { Board.new point}
+  subject { Board.new dim}
+  let( :dim ) { [1, 1] }
   let( :point ) { [0, 0] }
   let( :ship) do
     double 'ship', size: 1
   end
 
   it 'has a dimension' do
-    expect( subject.dimension).to eq point
+    expect( subject.dimension).to eq dim
+  end
+
+  it 'creates a blank board filled with water' do
+    expect(subject.blank).to eq({[0,0] => 'water'})
+
   end
 
   describe '#place' do
@@ -18,8 +24,9 @@ describe Board do
       expect( subject ).to respond_to( :place ).with( 2 ).arguments
     end
 
-    it 'adds a ship to the board' do
-      expect(subject.place ship, point).to eq ship
+    it 'adds a ship to the board at the given coordinate' do
+      subject.place ship,point
+      expect(subject.playing_on[point]).to eq ship
     end
 
   end
@@ -27,6 +34,11 @@ describe Board do
   describe '#ship?' do
     it 'responds to ship?' do
       expect( subject).to respond_to :ship?
+    end
+
+    it 'checks wether there is not water at a given coordinate' do
+      subject.place ship, point
+      expect(subject.ship? point).to be true
     end
   end
 
