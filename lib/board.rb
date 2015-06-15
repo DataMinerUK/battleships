f61b2085a2ec38870a928145dca862ab5c2f0b13
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-15 17:24:23 +0100

    Refactoring to check ship is on grid

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,5 @@
+require_relative 'ship'
+
 class Board
 
   attr_reader :dimension
@@ -8,19 +10,27 @@ class Board
     @playing_on = create_blank_board
   end
 
-  def place ship, coordinate, direction
-    playing_on[coordinate] = ship
+  def place ship, coordinate, orientation
+    orientation == 'horizontal' ? index = 1 : index = 0
+    ship.size.times do
+      playing_on[coordinate] = ship
+      coordinate[index] += 1
+    end
   end
 
   def ship_at? coordinate
     playing_on[coordinate] != :water
   end
 
+  def where_ship ship
+    playing_on.select{ |coord, value| value == ship }.keys
+  end
+
   private
 
   def create_blank_board
     # Making a Hash with all the coordinates of the board as keys setting them to value 'water'
-    coordinate_keys  = (0..@dimension[0]-1).to_a.product((0..@dimension[1]-1).to_a)
+    coordinate_keys  = (0..dimension[0]-1).to_a.product((0..dimension[1]-1).to_a)
     value_water = [:water] * coordinate_keys.count
     Hash[coordinate_keys.zip(value_water)]
   end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,7 +5,6 @@ describe Board do
   subject { Board.new dim}
   let( :dim ) { [1, 1] }
   let( :point ) { [0, 0] }
-  let( :orientation ) { :horizontal }
   let( :ship) do
     double 'ship', size: 1
   end
@@ -21,24 +20,47 @@ describe Board do
 
   describe '#place' do
 
+    subject { Board.new dim}
+    let( :dim ) { [5, 5] }
+    let( :orientation ) { 'horizontal' }
+    let( :ship) do
+      double 'ship', size: 3
+    end
+    before(:each) do
+      subject.place ship, point, orientation
+    end
+
     it 'responds to place with 3 arguments' do
       expect( subject ).to respond_to( :place ).with( 3 ).arguments
     end
 
     it 'adds a ship to the board at the given coordinate' do
-      subject.place ship, point, orientation
-      expect(subject.playing_on[point]).to eq ship
+      p subject.playing_on
+      expect(subject.playing_on[[0,0]]).to eq ship
+      expect(subject.playing_on[[0,1]]).to eq ship
+      expect(subject.playing_on[[0,2]]).to eq ship
+      # expect(subject.playing_on[[point[0],point[1]+1]]).to eq ship
+      # expect(subject.playing_on[[point[0],point[1]+2]]).to eq ship
+
     end
 
   end
 
-  describe '#ship?' do
+  # describe '#where_ship' do
+  #   it 'gives the coordinates of a ship' do
+  #     expect(subject.where_ship ship).to eq [[0,0],[0,1],[0,2]]
+  #
+  #   end
+  #
+  # end
+
+  describe '#ship_at?' do
     it 'responds to ship?' do
       expect( subject).to respond_to :ship_at?
     end
 
-    it 'checks wether there is not water at a given coordinate' do
-      subject.place ship, point, orientation
+    xit 'checks wether there is not water at a given coordinate' do
+      subject.place ship, point
       expect(subject.ship_at? point).to be true
     end
   end
