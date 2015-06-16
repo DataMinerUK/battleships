6b247b6bec33969cf4d4ae1f1f157a9a338f29e3
Alex <aleksandernasr@gmail.com>
2015-06-16 17:16:45 +0100

    Player reports misses when shot misses

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,12 @@
 class Player
 
   attr_reader :hits
+  attr_reader :misses
 
   def initialize board
     @board = board
     @hits = []
+    @misses = []
   end
 
   # def place ship
@@ -16,9 +18,13 @@ class Player
   end
 
   def receive_strike coordinate
-    hits << coordinate
-    @board.strike coordinate
+    shot = @board.strike coordinate
+    if shot == 'HIT!' 
+      hits << coordinate
+    else shot == 'MISS!'
+      misses << coordinate
+    end
+    shot
   end
-
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -13,25 +13,38 @@ describe Player do
   context 'playing on a board with a ship at C4' do
 
     ## when testing the player, TEST THE PLAYER = stubbing the board with
-    ## 'strike' is not a problem here
+    ## 'strike' is not a problem here, and you have to do it
+    ## because receive_strike is calling the method "strike" on the board
 
-    let(:board) {double :board, strike: 'HIT!'}
+    let(:board) { double :board, strike: 'HIT!' }
     subject { Player.new(board) }
 
     it 'can tell us when a ship is hit' do
       expect(subject.receive_strike "C4").to eq 'HIT!'
     end
 
-    it 'can report hit positions' do
-      subject.receive_strike "C4"
-      expect(subject.hits).to eq ["C4"]
+    context 'when player hits a ship' do
+
+      it 'can report hit positions' do
+        subject.receive_strike "C4"
+        expect(subject.hits).to eq ["C4"]
+      end
+
+    end
+    
+    context 'when player misses a ship' do
+
+      let(:board) { double :board, strike: 'MISS!'} 
+      subject { Player.new(board) }
+      
+      it 'can report miss positions' do
+        subject.receive_strike "C5"
+        expect(subject.misses).to eq ["C5"]
+      end
+
     end
 
   end
 
-  # it 'can place ships' do
-  #   subject.place ship
-
-  # end
 
 end
