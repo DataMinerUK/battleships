1cd0bb3c208a62b68750f163b70398612b527464
Alex <aleksandernasr@gmail.com>
2015-06-17 15:43:55 +0100

    Alex added comments

--- lib/board.rb
+++ lib/board.rb
@@ -12,6 +12,18 @@ class Board
   end
 
   def strike coordinate
+    
+    # board.map {|ship| ship.position} mapping out the positions of the ships in the board array [the mapped array is an array of arrays with each ship locations as an element] because we've specified ship.position as the parameter
+    # we need to use flatten in order for the mapped out "array of arrays" that is being produced with ship coordinates to become a single array containing ALL coordinates as individual elements
+    # we are checking with "include?" if the coordinate that is being struck is present in the mapped out array, and the include statement returns true/false
+    # if it is true, the next line of code will execute
+    # board.select will select a ship from the board array based on a condition, in this case the coordinate we are passing as an argument.
+    # board.select is pulling out each ship from the board array, if the ship's position is included in the coordinate given, it will SELECT it and create an array with that result.
+    # the reason the [0] is there at the end is because an array is produced and you must .hit the first (and only) element
+
+    # select returns |ship| based on the condition given after it
+    # map is going to look at each |ship| and return ship.position
+
     if board.map {|ship| ship.position}.flatten.include? (coordinate)
        board.select {|ship| ship.position.include?(coordinate)}[0].hit
       "HIT!"
@@ -23,10 +35,6 @@ class Board
 
   def all_ships_sunk?
     board.all? {|ship| ship.status == :sunk}
-    #    'You win! All ships have been sunk BIOTCH'
-    #  else
-    #    "Keep playing. Ships be floatin'"
-    #  end
   end
 
 
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -20,24 +20,24 @@ describe Player do
     ## when testing the player, TEST THE PLAYER = stubbing the board with
     ## 'strike' is not a problem here, and you have to do it
     ## because receive_strike is calling the method "strike" on the board
-    GIVEN_LOCATION = "C4"
+    LOCATION = "C4"
     let(:board) { double :board, strike: 'HIT!' }
     subject { Player.new(board) }
 
     it 'can tell us when a ship is hit' do
-      expect(subject.receive_strike GIVEN_LOCATION).to eq 'HIT!'
+      expect(subject.receive_strike LOCATION).to eq 'HIT!'
     end
 
     it 'raises an error when hitting the same place twice' do
-      subject.receive_strike GIVEN_LOCATION
-      expect {subject.receive_strike GIVEN_LOCATION}.to raise_error "You have already shot here"
+      subject.receive_strike LOCATION
+      expect {subject.receive_strike LOCATION}.to raise_error "You have already shot here"
     end
 
     context 'when player hits a ship' do
 
       it 'can report hit positions' do
-        subject.receive_strike GIVEN_LOCATION
-        expect(subject.hits).to eq [GIVEN_LOCATION]
+        subject.receive_strike LOCATION
+        expect(subject.hits).to eq [LOCATION]
       end
 
     end
@@ -53,8 +53,8 @@ describe Player do
       end
 
       it 'raises an error when missing the same place twice' do
-        subject.receive_strike GIVEN_LOCATION
-        expect{subject.receive_strike GIVEN_LOCATION}.to raise_error "You have already shot here"
+        subject.receive_strike LOCATION
+        expect{subject.receive_strike LOCATION}.to raise_error "You have already shot here"
       end
 
     end
